117e3f8e5d324bb432a14e27a6c8621365e53fb4
muduzen <mehmetutkuduzen@gmail.com>
2020-02-23 02:03:10 +0300

    a uselss late commit for the mapify function

--- HoleyKrueger/RandomMap.gd
+++ HoleyKrueger/RandomMap.gd
@@ -8,8 +8,45 @@ extends Node2D
 func generate(sizex, sizey):
 	for x in range(sizex):
 		for y in range(sizey):
-			$TileMap.set_cell(x,y, randi()%2);
+			$TileMap.set_cell(x,y, randi()%2); 
+#	mapify(sizex, sizey, 3, 0.6)		
 
+# to create smoother maps
+func mapify(sizex, sizey, n, t):
+	for i in range(n):
+		for x in range(sizex):
+			for y in range(sizey):
+				var tot = 1
+				var one = $TileMap.get_cell(x,y)
+				if x != 0 and y != 0:
+					tot += 1
+					one += $TileMap.get_cell(x-1, y-1)
+				if x != 0:
+					tot += 1
+					one += $TileMap.get_cell(x-1, y)					
+				if y != 0:
+					tot += 1
+					one += $TileMap.get_cell(x, y-1)
+				if x != sizex-1 and y != 0:
+					tot += 1
+					one += $TileMap.get_cell(x+1, y-1)
+				if x != sizex-1 and y != sizey-1:
+					tot += 1
+					one += $TileMap.get_cell(x+1, y+1)										
+				if x != 0 and y != sizey-1:
+					tot += 1
+					one += $TileMap.get_cell(x-1, y+1)
+				if x != sizex-1:
+					tot += 1
+					one += $TileMap.get_cell(x+1, y)										
+				if y != sizey-1:
+					tot += 1
+					one += $TileMap.get_cell(x, y+1)
+				if float(one)/float(tot) >= t:
+					$TileMap.set_cell(x, y, 1)
+				else:
+					$TileMap.set_cell(x, y, 0)							
+												
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize();
